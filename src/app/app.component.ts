aadc77dd6025891731549d076a7eefe4b54623fb
Olga Volobueva <olvik1605@gmail.com>
2025-04-08 22:16:35 -0400

    feat: add DasboardItem component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,18 @@ import { HeaderComponent } from './components/header/header.component';
 import { ServerStatusComponent } from './components/dashboard/server-status/server-status.component';
 import { TrafficComponent } from './components/dashboard/traffic/traffic.component';
 import { TicketsComponent } from './components/dashboard/tickets/tickets.component';
+import { DashboardItemComponent } from './components/dashboard/dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent],
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    TicketsComponent,
+    DashboardItemComponent,
+  ],
 })
 export class AppComponent {}
